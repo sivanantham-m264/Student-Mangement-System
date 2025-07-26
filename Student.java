1d5396548d61168ba6c029ec8150e193cdeba044
sivanantham-m264 <sivanantham.m264@gmail.com>
2025-07-26 23:20:33 +0530

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,13 @@
+public class Student {
+    int id;
+    String name;
+    int age;
+    String course;
+
+    public Student(int id, String name, int age, String course) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.course = course;
+    }
+}
